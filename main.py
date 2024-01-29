a057bfe21e4975577da440d234d09164db5aa84e
Ahsan Awadullah Azizan <ahsanaz461@gmail.com>
2024-01-29 11:07:08 +0700

    Add copy-to-clipboard feature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Google drive image embedder
 
 import webbrowser as wb
+import pyperclip as clip
 
 if __name__ == "__main__":
     print("The content must have a valid ID and must have public access")
@@ -13,6 +14,9 @@ if __name__ == "__main__":
 
     # Tweak the link to https://lh3.google.com/u/0/d/[id]
     tweaked_link = f'https://lh3.google.com/u/0/d/{content_id}'
+
+    # Copy to clipboard
+    clip.copy(tweaked_link)
     print(f'URL: {tweaked_link}')
 
     wb.open(tweaked_link)
